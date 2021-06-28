0051fb07af231d3e657e5507749b03e5371dfad0
Lloyd Blake <treleven.lloyd@gmail.com>
2021-06-28 14:57:11 +0100

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
+  ]);
+  return (
+    <div className='home'>
+      <h2>Homepage</h2>
+    </div>
+  );
+};
+
+export default Home;
