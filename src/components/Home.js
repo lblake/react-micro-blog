7d514c1f79f04e43a000c4b20c258954bd016f36
Lloyd Blake <treleven.lloyd@gmail.com>
2021-06-29 15:41:03 +0100

    Added BlogList component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BlogList from './BlogList'
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -8,7 +9,7 @@ const Home = () => {
   ]);
   return (
     <div className='home'>
-      <h2>Homepage</h2>
+     <BlogList blogs={blogs} title="All Blogs!"/>
     </div>
   );
 };
