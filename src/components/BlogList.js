7413a4eeb0313d85b28f379df75712d74e7f81db
Lloyd Blake <treleven.lloyd@gmail.com>
2021-06-29 21:58:08 +0100

    Added code to allow deletion of blogs

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,11 +1,13 @@
-const BlogList = ({blogs, title}) => {
+const BlogList = ({blogs, title,handleDelete}) => {
   return (
     <div className='blog-list'>
         <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className='blog-preview' key={blog.id}>
           <h2>{blog.title}</h2>
-          <p>{blog.author}</p>
+          <p> "Written By" {blog.author} </p>
+          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
+          
         </div>
       ))}
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import BlogList from './BlogList'
+import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -7,9 +7,14 @@ const Home = () => {
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
   ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  };
   return (
     <div className='home'>
-     <BlogList blogs={blogs} title="All Blogs!"/>
+      <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete} />
     </div>
   );
 };
